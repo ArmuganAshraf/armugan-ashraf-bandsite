fe9734a4d49c3d11d14ee71ea2762ccc00c20dfa
Armugan Ashraf <>
2019-07-21 18:23:11 -0400

    fixed breakpoints for the bio page

--- Sprint3/shows.js
+++ Sprint3/shows.js
@@ -1,16 +1,14 @@
 const apiKey  = '6729f518-b29e-4f0f-959b-ee5dafb5b5fd';
 
 window.addEventListener('load',() => {
-  console.log('doc is ready');
+  console.log('doc is ready');// used this part from the API class notes
 
   axios.get('https://project-1-api.herokuapp.com/showdates?api_key='+apiKey)
   .then(function (response) {
-    // handle success
     console.log(response.data);
     showDates(response.data);
   })
   .catch(function (error) {
-    // handle error
     console.log(error);
   });
 });
